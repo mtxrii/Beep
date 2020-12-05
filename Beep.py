348f2b6de631649cd3ae85559d028d4bac66873b
Enrique <edavalos@ucsc.edu>
2020-12-04 19:52:51 -0800

    Skeleton code

--- Beep.py
+++ Beep.py
@@ -1,2 +1,14 @@
 while True:
-    cmd = input('What would you like to do?')
+    print("Hello. Welcome to Beep. Read or write?")
+    print("+-------------------------------------+")
+    cmd = input('(Read|Write) -> ').upper()
+
+    if cmd == 'READ':
+        print('Reading...')
+
+    elif cmd == 'WRITE':
+        print('Writing...')
+
+    else:
+        print('Unknwon command. Exiting...')
+        break
