30bc7edeb107548cd8fafd4e87d09f3ce53a591d
Enrique <edavalos@ucsc.edu>
2020-12-05 19:43:12 -0800

    Added more skeleton

--- Beep.py
+++ Beep.py
@@ -1,18 +1,27 @@
-filename = ''
+filename_in = ''
+filename_out = ''
 
 while True:
     print("Hello. Welcome to Beep. This program reads from and writes to .bb files. If you choose to write, it will\n"
-          "take any text file and convert it to a .bb file. If you choose to read, it will take any text file and\n"
-          "translate it to a .bb file.")
-    print("+-------------------------------------+")
+          "take any text file and convert it to a .bb file. If you choose to read, it will take any .bb file and\n"
+          "translate it to a .txt file.")
+    print("+------------------------------------------------------------------------------------------------------+")
     cmd = input('(Read|Write) -> ').upper()
 
     if cmd == 'READ':
-        print('Reading...')
+        filename_in = input('File to translate -> ')
+
+
+        filename_out = input('File to output translation -> ')
+
 
     elif cmd == 'WRITE':
-        filename = input('What file would you like to write to? -> ')
+        filename_in = input('File to encode -> ')
+
+
+        filename_out = input('File to output encoding -> ')
 
     else:
-        print('Unknwon command. Exiting...')
+        print('+------------------------------------------------------------------------------------------------------+\n'
+              'Unknwon command. Exiting...')
         break
